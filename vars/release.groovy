9a06b44d8307e7ddb2911cc320d3b464c698b5aa
quickfix <devops.july2.2017@gmail.com>
2018-09-03 00:11:13 +0530

    Update release.groovy

--- vars/release.groovy
+++ vars/release.groovy
@@ -10,7 +10,7 @@ def call(String name = 'human') {
                      file: '/home/vagrant/jenkins/workspace/test-artifact/target/simple-maven-project-with-tests-1.0-SNAPSHOT.jar',
                      type: 'jar'
                    ]],
-                   credentialsId: '',
+                   credentialsId: 'spring-user',
                    groupId: 'test',
                    nexusUrl: '192.168.33.30:8081/nexus',
                    nexusVersion: 'nexus2',
