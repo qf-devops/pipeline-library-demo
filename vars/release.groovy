d922de0d5ebb396c2b2e7a0e64341ecea1a70448
quickfix <devops.july2.2017@gmail.com>
2018-09-02 14:56:39 +0530

    Update release.groovy

--- vars/release.groovy
+++ vars/release.groovy
@@ -3,20 +3,5 @@
 
 def call(String name = 'human') {
   echo "Hello, ${name}."
-  nexusArtifactUploader {
-        nexusVersion('nexus2')
-        protocol('http')
-        nexusUrl('192.168.33.30:8081/nexus')
-        groupId('test')
-        version('2.4')
-        repository('spring-snapshots')
-        credentialsId('44620c50-1589-4617-a677-7563985e46e1')
-        artifact {
-            artifactId('simple-maven-project-with-tests')
-            type('jar')
-            
-            file('simple-maven-project-with-tests.jar')
-        }
-        
-      }
+  
 }
