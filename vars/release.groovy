b8b694fe5a1d3a862c7b6449f06fe83030613da5
quickfix <devops.july2.2017@gmail.com>
2018-09-02 16:33:54 +0530

    Update release.groovy

--- vars/release.groovy
+++ vars/release.groovy
@@ -5,17 +5,17 @@ def call(String name = 'human') {
   echo "Hello, ${name}."
   nexusArtifactUploader(
                    artifacts: [[
-                     artifactId: simple-maven-project-with-tests,
+                     artifactId: 'simple-maven-project-with-tests',
                      classifier: '',
                      file: '/home/vagrant/jenkins/workspace/test-artifact/target/simple-maven-project-with-tests-1.0-SNAPSHOT.jar',
-                     type: ‘jar'
+                     type: 'jar'
                    ]],
                    credentialsId: '974f8103-66ef-4748-accf-2ce62f3602f9',
-                   groupId: ’test',
+                   groupId: 'test',
                    nexusUrl: '192.168.33.30:8081/nexus',
                    nexusVersion: 'nexus2',
                    protocol: 'http',
-                   repository: ’spring-snapshots',
-                   version: 1.0-SNAPSHOT
+                   repository: 'spring-snapshots',
+                   version: '1.0-SNAPSHOT'
                    ) 
 }
